66d087386a6de7b386d1f0f106a9c72cc0c08014
tsai <lovez81427@gmail.com>
2024-01-26 13:56:48 +0800

    index增加註解

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
     <title>健康促進網</title>
-    <link href="./css/css.css" rel="stylesheet" type="text/css">
-    <script src="./js/jquery-1.9.1.min.js"></script>
-    <script src="./js/js.js"></script>
-</head>
+    <link href="./css/css.css" rel="stylesheet" type="text/css"> <!-- -->
+    <script src="./js/jquery-1.9.1.min.js"></script> <!-- -->
+    <script src="./js/js.js"></script> <!-- -->
+</head> 
+<!-- -->
 <style>
     .pop {
         background: rgba(51, 51, 51, 0.8);
@@ -27,14 +28,17 @@
 <body>
     <div id="all">
         <div id="title">
-            <?= date("m月d日 1"); ?>|
-            今日瀏覽:<?= $Total->find(['date' => date("Y-m-d")])['total']; ?>
-            累積瀏覽:<?= $Total->sum('total'); ?>
-            <a href="index.php" style='float:right'>回首頁</a> <!--float:right這個元素將浮動到右側。 -->
+            <!--這段要寫 -->
+            <?= date("m月d日 1"); ?>| 
+            今日瀏覽:<?= $Total->find(['date' => date("Y-m-d")])['total']; ?>  
+            累積瀏覽:<?= $Total->sum('total'); ?>  
+            <a href="index.php" style='float:right'>回首頁</a> <!--float:right這個元素將浮動到右側。 --> 
         </div>
-        <div id="title2" title="健康促進網-回首頁">
-            <a href="index.php"><img src="./icon/02B01.jpg" alt=""></a> <!-- -->
+        <div id="title2" title="健康促進網-回首頁">  
+            <a href="index.php"><img src="./icon/02B01.jpg" alt=""></a> 
         </div>
+        <!--這段要寫 -->
+
         <div id="mm">
             <div class="hal" id="lef">
                 <a class="blo" href="?do=po">分類網誌</a>
@@ -44,30 +48,35 @@
                 <a class="blo" href="?do=que">問卷調查</a>
             </div>
             <div class="hal" id="main">
-                <div>
-                    <marquee style="width:80%;display:inline-block">
-                        請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章
-                    </marquee>
+                <div> 
+                    <!--這段要寫 -->
+                    <marquee style="width:80%;display:inline-block"> 
+                        請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章 
+                    </marquee> <!--這段要寫 -->
+
+                    <!--這段要寫 -->
                     <span style="width:20%;display:inline-block;">
-                        <?php
-                        if (!isset($_SESSION['user'])) {
+                        <?php 
+                        if (!isset($_SESSION['user'])) {  
+                        ?>  
+                            <a href="?do=login">會員登入</a> 
+                        <?php 
+                        } else { 
                         ?>
-                            <a href="?do=login">會員登入</a>
-                        <?php
-                        } else {
-                        ?>
-                            歡迎,<?= $_SESSION['user']; ?>
-                            <button onclick="location.href='./api/logout.php'">登出</button>
-                            <?php
-                            if ($_SESSION['user'] == 'admin') {
-                            ?>
-                                <button onclick="location.href='back.php'">管理</button>
+                            歡迎,<?= $_SESSION['user']; ?>  
+                            <button onclick="location.href='./api/logout.php'">登出</button> 
+                            <?php 
+                            if ($_SESSION['user'] == 'admin') { 
+                            ?>  
+                                <button onclick="location.href='back.php'">管理</button> 
                         <?php
                             }
                         }
                         ?>
-                    </span>
-                    <div class="">
+                    </span> <!--這段要寫 -->
+
+                    <!--這段要寫 -->
+                    <div class=""> 
                         <?php
                         // 获取请求中的 'do' 参数，如果不存在则默认为 'main'
                         $do = $_GET['do'] ?? 'main';
@@ -88,6 +97,7 @@
                 </div>
             </div>
         </div>
+        <!--這段要寫 -->
         <div id="bottom">
             本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
             <br>
