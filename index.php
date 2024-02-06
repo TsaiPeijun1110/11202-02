40680a44badf9c5fbac6b5570884ee97257bbb04
tsai <lovez81427@gmail.com>
2024-02-06 09:01:04 +0800

    新增total跟user資料表

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link href="./css/css.css" rel="stylesheet" type="text/css"> <!-- -->
     <script src="./js/jquery-1.9.1.min.js"></script> <!-- -->
     <script src="./js/js.js"></script> <!-- -->
-</head> 
+</head>
 
 <!--這段要寫--開頭 -->
 <style>
@@ -33,15 +33,279 @@
         <div id="title">
 
             <!--這段要寫--開頭 -->
-            <?= date("m月d日 1"); ?>| 
-            今日瀏覽:<?= $Total->find(['date' => date("Y-m-d")])['total']; ?>  
-            累積瀏覽:<?= $Total->sum('total'); ?>  
-            <a href="index.php" style='float:right'>回首頁</a> <!--float:right這個元素將浮動到右側。 --> 
+            <?= date("m月d日 1"); ?>|
+            今日瀏覽:<?= $Total->find(['date' => date("Y-m-d")])['total']; ?>
+            累積瀏覽:<?= $Total->sum('total'); ?>
+            <a href="index.php" style='float:right'>回首頁</a> <!--float:right這個元素將浮動到右側。 -->
         </div>
-        <div id="title2" title="健康促進網-回首頁">  
-            <a href="index.php"><img src="./icon/02B01.jpg" alt=""></a> 
+        <div id="title2" title="健康促進網-回首頁">
+            <a href="index.php"><img src="./icon/02B01.jpg" alt=""></a>
         </div>
         <!--這段要寫--結束 -->
+
+        <!-----要刪掉 ----->
+        <? date("m月d日 1"); ?> |
+        今日瀏覽:<?= $Total->find(['date' => date("Y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float:right">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <a href="index.php"><img src="./icon/02B01.jpg" alt=""></a>
+        </div>
+
+        <?= date("m月d日1") ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['total']; ?> |
+        累積瀏覽:<?= $Ttoal->sum('total'); ?>
+        <a href="index.php" style="float:light">回首頁</a>
+        <div id="title" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float:right">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['ttoal']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">回首頁</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B02.jpg" alt="">
+        </div>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">登入</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">登入</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">登入</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum['total']; ?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">登入</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">會員登入</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理員登入</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">登入</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理員登入</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">回首頁
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">登入</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理員登入</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?= date("m月d日 1"); ?>
+        今日瀏覽:<?= $Total->find(['date' => ("Y-m-d")])['total']; ?>
+        累積瀏覽:<?= $Total->sum('total'); ?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+        </div>
+
+        <?php
+        if (!isset($_SESSION['user'])) {
+        ?>
+            <a href="?do=login">登入</a>
+        <?php
+        } else {
+        ?>
+            歡迎,<?= $_SESSION['user']; ?>
+            <button onclick="location.href='./api/logout.php'">登出</button>
+            <?php
+            if ($_SESSION['user'] == 'admin') {
+            ?>
+                <button onclick="location.href='back.php'">管理</button>
+        <?php
+            }
+        }
+        ?>
+
+        <?=date("m月d日 1");?>
+        今日瀏覽:<?=$Total->find(['date'=>date("Y-m-d")])['total'];?>
+        累積瀏覽:<?=$Total->sum('total');?>
+        <a href="index.php" style="float: right;">回首頁</a>
+        <div id="title2" title="健康促進網-回首頁">
+            <img src="./icon/02B01.jpg" alt="">
+
+        
+        </div>
+        <!-----要刪掉 ----->
 
         <div id="mm">
             <div class="hal" id="lef">
@@ -52,37 +316,213 @@
                 <a class="blo" href="?do=que">問卷調查</a>
             </div>
             <div class="hal" id="main">
-                <div> 
+                <div>
                     <!--這段要寫--開頭 -->
-                    <marquee style="width:80%;display:inline-block"> 
-                        請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章 
+                    <marquee style="width:80%;display:inline-block">
+                        請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章
                     </marquee> <!--這段要寫--結束 -->
 
 
                     <!--這段要寫--開頭 -->
                     <span style="width:20%;display:inline-block;">
-                        <?php 
-                        if (!isset($_SESSION['user'])) {  
-                        ?>  
-                            <a href="?do=login">會員登入</a> 
-                        <?php 
-                        } else { 
+                        <?php
+                        if (!isset($_SESSION['user'])) {
                         ?>
-                            歡迎,<?= $_SESSION['user']; ?>  
-                            <button onclick="location.href='./api/logout.php'">登出</button> 
-                            <?php 
-                            if ($_SESSION['user'] == 'admin') { 
-                            ?>  
-                                <button onclick="location.href='back.php'">管理</button> 
+                            <a href="?do=login">會員登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
                         <?php
                             }
                         }
                         ?>
+                        <!--這段要寫--結束 -->
+
+                        <!-----要刪掉 ----->
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login">登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login">登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login">登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= ($_SESSION['user']); ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login">登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+
+
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login.php">登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+
+
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login">登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login">登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login">登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+                        <?php
+                        if (!isset($_SESSION['user'])) {
+                        ?>
+                            <a href="?do=login">會員登入</a>
+                        <?php
+                        } else {
+                        ?>
+                            歡迎,<?= $_SESSION['user']; ?>
+                            <button onclick="location.href='./api/logout.php'">登出</button>
+                            <?php
+                            if ($_SESSION['user'] == 'admin') {
+                            ?>
+                                <button onclick="location.href='back.php'">管理員登入</button>
+                        <?php
+                            }
+                        }
+                        ?>
+
+
+
+                        <!-----要刪掉 ----->
+
+
                     </span> <!--這段要寫--結束 -->
 
 
                     <!--這段要寫--開頭 -->
-                    <div class=""> 
+                    <div class="">
                         <?php
                         // 获取请求中的 'do' 参数，如果不存在则默认为 'main'
                         $do = $_GET['do'] ?? 'main';
@@ -101,16 +541,21 @@
                         ?>
                         <!--這段要寫--結束 -->
 
+                        <!-----要刪掉 ----->
+                        
+
+
+                        <!-----要刪掉 ----->
                     </div>
                 </div>
             </div>
         </div>
-        
+
         <div id="bottom">
             本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
             <br>
             服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
-        </div>                                          <!-- 照片連結路徑要改成icon-->
+        </div> <!-- 照片連結路徑要改成icon-->
     </div>
 
 </body>
